9e544a2fc1f3112d638efbbe106eaa54294c2573
Santiago Bobadilla <s.bobadilla@uniandes.edu.co>
2021-11-11 20:25:54 -0500

    Yeah

--- Stigmergy_V2/Pedido/views.py
+++ Stigmergy_V2/Pedido/views.py
@@ -83,7 +83,7 @@ def change_orden(request, pk):
 def orden_create(request):
 
     role = getRole(request)
-    if role != "Big Boss" or role != "User":
+    if role != "Big Boss" or role != "Usuario Comprador":
         return HttpResponse("Unauthorized User")
 
     if request.method == 'POST':
